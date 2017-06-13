cfad5ed69a89b23c02987eefaee180fdb754ca8e
zhangxianhongx <ybon@ybondeiMac-4.local>
2017-06-13 17:33:29 +0800

    提交模板

---
+++ Bmob/Bmob/BmobUser.swift
@@ -0,0 +1,116 @@
+//
+//  BmobUser.swift
+//  Bmob
+//
+//  Created by ybon on 2017/6/13.
+//  Copyright © 2017年 ybon. All rights reserved.
+//
+
+import UIKit
+
+class BmobUser: NSObject {
+
+   
+    private var UserInfo = Dictionary<String,Any>();
+    //设置用户名
+    func setName(userName:String){
+        
+        UserInfo["username"] = userName;
+    }
+    //设置密码
+    func setPassWord(password:String){
+        UserInfo["password"] = password;
+    }
+    //设置其它属性
+    func setUserInfo(value:Any,key:String){
+        UserInfo[key] = value;
+    }
+    /**
+     注册用户
+     */
+    func register(mathFunction:@escaping (_ responObject:AnyObject,_ isSuccess:Bool,_ zyError:Error?)->Void){
+        
+        if UserInfo["username"] == nil || UserInfo["password"] == nil {
+            mathFunction("用户名或密码缺失" as AnyObject,false,nil);
+            return;
+        }
+        let url = BaseUserUrl + "users";
+        BmobRequestWork.zyRowPOSTwithURLSession(url, parmas: UserInfo) { (anyObject, isSuccess, zyError) in
+            mathFunction(anyObject,isSuccess,zyError);
+        }
+   
+    }
+    
+    /**
+     登录用户
+     */
+    func login(username:String,password:String,mathFunction:@escaping (_ responObject:AnyObject,_ isSuccess:Bool,_ zyError:Error?)->Void){
+        if username == "" || password == "" {
+            mathFunction("用户名或密码缺失" as AnyObject,false,nil);
+            return;
+        }
+        let url = BaseUserUrl + "login"
+        BmobRequestWork.zyGETWithURLSession(url, parmas: ["username":username,"password":password]) { (anyObject, isSuccess, zyError) in
+            if isSuccess{
+                
+                BmobSDK.shareBmobSDK().currentUserID = anyObject["objectId"] as? String;
+                BmobSDK.shareBmobSDK().sessionToken = anyObject["sessionToken"] as? String;
+            }
+            mathFunction(anyObject,isSuccess,zyError);
+        }
+    }
+    /**
+     获取当前用户信息
+     */
+    func currentUser(mathFunction:@escaping (_ responObject:AnyObject,_ isSuccess:Bool,_ zyError:Error?)->Void){
+        if BmobSDK.shareBmobSDK().currentUserID == nil {
+            mathFunction("未登录" as AnyObject,false,nil);
+            return ;
+        }
+        let url = BaseUserUrl + "users/" + BmobSDK.shareBmobSDK().currentUserID!;
+        BmobRequestWork.zyGETWithURLSession(url, parmas: NSDictionary()) { (anyObject, isSuccess, zyError) in
+          
+            mathFunction(anyObject,isSuccess,zyError);
+        }
+    }
+    /**
+     更新当前用户信息
+     */
+    func upDateUesrInfo(dataDic:Dictionary<String,Any>,mathFunction:@escaping (_ responObject:AnyObject,_ isSuccess:Bool,_ zyError:Error?)->Void){
+        
+        let url = BaseUserUrl + "users/" + BmobSDK.shareBmobSDK().currentUserID!;
+        BmobRequestWork.zyUpwithURLSession(url, parmas: dataDic) { (anyObject, isSuccess, zyError) in
+            mathFunction(anyObject,isSuccess,zyError);
+        }
+    }
+    /**
+     查询所有用户
+     searchDic 查询的限制
+     如["limit":"12","skip":"10"]设置返回数据个数和跳过多少条数据
+     */
+    func searchAllUser(searchDic:Dictionary<String,String>,mathFunction:@escaping (_ responObject:AnyObject,_ isSuccess:Bool,_ zyError:Error?)->Void){
+        let url = BaseUserUrl + "users" ;
+        BmobRequestWork.zyGETWithURLSession(url, parmas: searchDic as NSDictionary) { (anyObject, isSuccess, zyError) in
+            
+            mathFunction(anyObject,isSuccess,zyError);
+        }
+    }
+    /**
+     修改密码
+     新老密码方式修改
+     */
+    func updatePassword(oldPassword:String,newPassword:String,mathFunction:@escaping (_ responObject:AnyObject,_ isSuccess:Bool,_ zyError:Error?)->Void){
+        
+        if oldPassword == "" || newPassword == "" || oldPassword == newPassword{
+            mathFunction("密码内容有误" as AnyObject,false,nil);
+            return;
+        }
+        let url = BaseUserUrl + "updateUserPassword/" + BmobSDK.shareBmobSDK().currentUserID!;
+        
+       BmobRequestWork.zyUpwithURLSession(url, parmas:["oldPassword":oldPassword,"newPassword":newPassword]) { (anyObject, isSuccess, zyError) in
+         mathFunction(anyObject,isSuccess,zyError);
+        }
+        
+    }
+    
+}
---
+++ Bmob/Bmob/Bridge.h
@@ -0,0 +1,9 @@
+//
+//  Bridge.h
+//  Bmob
+//
+//  Created by ybon on 2017/6/12.
+//  Copyright © 2017年 ybon. All rights reserved.
+//
+
+
